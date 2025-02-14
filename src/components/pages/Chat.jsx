de93d91e35d0a93763a5792b8a2e58533090c2cf
hassannn456 <hassanfzl11@gmail.com>
2025-02-14 08:06:05 -0500

    New fields for base chat API and fixed names

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -3,7 +3,7 @@ import { Box, Grid, IconButton, Stack } from "@mui/material";
 import { AutoAwesomeMosaic, Menu } from "@mui/icons-material";
 import { useAuth } from "@clerk/clerk-react";
 
-import axios, { setToken } from "../utils/axios";
+import apiClient, { setToken } from "../utils/axios";
 import SideBar, { StyledButton } from "../componentsExtended/Chat/SideBar";
 import InputField from "../componentsExtended/Chat/Input";
 import ChatMessages from "../componentsExtended/Chat/ChatMessages";
@@ -34,7 +34,7 @@ const Chat = () => {
     setErrorList(false);
     setLoadingList(true);
     try {
-      const response = await axios.get("/chat/list");
+      const response = await apiClient.get("/chat/list");
       setChatHistory(response.data);
       setLoadingList(false);
     } catch (error) {
@@ -50,7 +50,7 @@ const Chat = () => {
     setError(null);
     setLoading(true);
     try {
-      const response = await axios.get("/chat/get", {
+      const response = await apiClient.get("/chat/get", {
         params: {
           chat_id: chatId,
         },
@@ -96,7 +96,7 @@ const Chat = () => {
     setLoadingList(true);
     const fetchChatHistory = async () => {
       try {
-        const response = await axios.get("/chat/list");
+        const response = await apiClient.get("/chat/list");
         setChatHistory(response.data);
         setLoadingList(false);
       } catch (error) {
@@ -157,18 +157,26 @@ const Chat = () => {
     setInput("");
     setIsTyping(true);
     setError(null);
+    // const subscriptionId = JSON.parse(localStorage.getItem('subscriptionId'));
+    // if (!subscriptionId) {
+    //   setIsSubscribed(true);
+    //   return;
+    // }
 
     if (input.trim()) {
       setChat([...chat, { role: "user", content: input }]);
 
-      axios
+      apiClient
         .post("/chat/response", {
           chat_id: selected || "",
           message: input.trim(),
-          // data_sources: ["experiences", "hypotheses", "research"],
           // return_results: true,
-          // answer_model: "gpt-3.5-turbo",
+          // answer_model: "gpt-4o-mini",
           save: saveChat,
+          // ai_response: true,
+          // ai_query: true,
+          // search_data: true
+          // data_sources: ["experiences", "hypotheses", "research"],
         })
         .then((res) => {
           let response = res.data;
@@ -207,17 +215,17 @@ const Chat = () => {
               const updatedChatHistory = prevChatHistory.map((item) =>
                 item.chat_id === selected
                   ? {
-                      ...item,
-                      chat: [
-                        ...item?.chat,
-                        { role: "user", content: input },
-                        {
-                          role: "ai",
-                          content: response?.ai,
-                          db_results: response?.db_results,
-                        },
-                      ],
-                    }
+                    ...item,
+                    chat: [
+                      ...item?.chat,
+                      { role: "user", content: input },
+                      {
+                        role: "ai",
+                        content: response?.ai,
+                        db_results: response?.db_results,
+                      },
+                    ],
+                  }
                   : item
               );
 
